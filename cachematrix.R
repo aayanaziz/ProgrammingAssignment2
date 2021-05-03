976a11ac9dd1098cc54566b10cdb4fca80e57bbe
aayanaziz <aayan.aziz1@learner.manipal.edu>
2021-05-03 12:58:29 +0530

    initial commit

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,38 @@
 ## functions do
 
 ## Write a short comment describing this function
+## Writes functions for getting and hardcoding the
+## input matrix and inverted matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) i <<- inv
+  getinv <- function() i
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+  
 }
 
 
 ## Write a short comment describing this function
+## function checks if data is present in cache
+## if not then it calculates and returns the 
+## inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinv()
+  if(!is.null(i)) {
+    return(i)
+  }
+  m <- x$get()
+  i <- solve(m, ...)
+  x$setinv(i)
+  ## Return a matrix that is the inverse of 'x'
+  i
 }
